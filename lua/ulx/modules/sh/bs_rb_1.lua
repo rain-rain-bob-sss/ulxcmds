d2270f61197007a0e6e0fa872776dafa5b60fe8a
[TW]Rain_Bob <61649368+rain-rain-bob-sss@users.noreply.github.com>
2026-02-20 12:29:06 +0800

    Update bs_rb_1.lua

--- lua/ulx/modules/sh/bs_rb_1.lua
+++ lua/ulx/modules/sh/bs_rb_1.lua
@@ -1,4 +1,4 @@
-if engine.ActiveGamemode() ~= "bloodshed" then return end
+if not engine.ActiveGamemode():match("bloodshed") and not engine.ActiveGamemode():match("bsrrf") then return end
 
 function ulx.givemoney(caller, targets, points)
     for i = 1, #targets do
@@ -6,7 +6,7 @@ function ulx.givemoney(caller, targets, points)
         target:AddMoney(points)
     end
 
-    ulx.fancyLogAdmin(caller, "#A gave #i money to #T", points, targets)
+    ulx.fancyLogAdmin(caller, "#A gave $#i to #T", points, targets)
 end
 
 local givem = ulx.command("Bloodshed", "ulx givemoney", ulx.givemoney, "!givemoney")
@@ -26,7 +26,7 @@ function ulx.setguilt(caller, targets, guilt)
         target:SetNWFloat("Guilt", guilt)
     end
 
-    ulx.fancyLogAdmin(caller, "#A set #T's guilt to #i", target, guilt)
+    ulx.fancyLogAdmin(caller, "#A set #T's guilt to #i", targets, guilt)
 end
 
 local setguilt = ulx.command("Bloodshed", "ulx setguilt", ulx.setguilt, "!setguilt")
@@ -41,8 +41,8 @@ setguilt:addParam{
 }
 
 function ulx.settime(caller, time)
-    if not MuR.SetEndTime then return end
-    MuR:SetEndTime(CurTime() + time)
+    MuR.TimerEndTime = (CurTime() + time)
+    ulx.fancyLogAdmin(caller, "#A set the timer to #i", time)
 end
 
 local settime = ulx.command("Bloodshed", "ulx settime", ulx.settime, "!settime")
@@ -50,4 +50,42 @@ settime:defaultAccess(ULib.ACCESS_ADMIN)
 settime:addParam{
     type = ULib.cmds.NumArg,
     hint = "time"
+}
+
+function ulx.setrole(caller, targets, role)
+    for i = 1, #targets do
+        local target = targets[i]
+        local pos,ang = target:GetPos(),target:EyeAngles()
+        target.ForceSpawn = true
+        target:SetNW2String("Class", role)
+        target:Spawn()
+        target.ForceSpawn = false
+        target:SetPos(pos)
+        target:SetEyeAngles(ang)
+    end
+
+    ulx.fancyLogAdmin(caller, "#A set #T's role to #s", targets, role)
+end
+
+local setrole = ulx.command("Bloodshed", "ulx setrole", ulx.setrole, "!setrole")
+setrole:defaultAccess(ULib.ACCESS_ADMIN)
+setrole:addParam{
+    type = ULib.cmds.PlayersArg
+}
+
+local COMPLETES = {}
+
+timer.Create("Refresh MuR_Role COMPLETES", 0.5, 1, function()
+	table.Empty(COMPLETES)
+	for class in pairs(GAMEMODE:GetRoles()) do
+		COMPLETES[#COMPLETES + 1] = class
+	end
+	table.sort(COMPLETES,function(a,b) return b>a end)
+end)
+
+
+setrole:addParam{
+    type = ULib.cmds.StringArg,
+    hint = "role",
+    completes = COMPLETES
 }
